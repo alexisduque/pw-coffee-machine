80303828b4ac150f72e095e257c57e55eead08f4
Alexis Duque <alexisd61@gmail.com>
2016-06-05 17:12:53 +0200

    Refactoring and add BLE central implementation

---
+++ edison/coffee-ble-central/index.js
@@ -0,0 +1,39 @@
+var noble = require('noble');
+var chalk = require('chalk');
+var urldecode = require('./urldecode.js');
+var metadata = require('./metadata.js');
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning(['feaa']);
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('scanStart', function() {
+  console.log(chalk.dim('Scan started...'));
+  console.log();
+});
+
+noble.on('scanStop', function() {
+  console.log(chalk.dim('Scan stopped...'));
+  console.log();
+});
+
+noble.on('discover', function(peripheral) {
+  var serviceData = peripheral.advertisement.serviceData;
+  if (serviceData && serviceData.length) {
+    var objects = [];
+    for (var i in serviceData) {
+      // check if Eddystone-URL
+      if (serviceData[i].data.toString('hex').substr(0, 2) === '10') {
+        var url = urldecode(serviceData[i].data.toString('hex'));
+        objects.push({'url': url});
+      }
+    }
+    if (objects.length) {
+      metadata(objects);
+    }
+  }
+});
---
+++ edison/coffee-ble-central/metadata.js
@@ -0,0 +1,85 @@
+'use strict';
+var http = require('http'),
+  chalk = require('chalk'),
+  notifier = require('node-notifier');
+
+module.exports = function(urldata)  {
+  var urls = JSON.stringify({'objects': urldata}),
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': urls.length
+    },
+    options = {
+      'host': 'service.physical-web.org',
+      'port': 80,
+      'path': '/resolve-scan',
+      'method': 'POST',
+      'headers': headers
+    },
+    urlOnly = function() {
+      var data = JSON.parse(urls);
+      for (var i in data.objects) {
+        if (data.objects.hasOwnProperty(i)) {
+          console.log(chalk.underline.bgBlue(' ' + data.objects[i].url + ' '));
+          console.log(data.objects[i].url);
+
+          notifier.notify({
+            'title': data.objects[i].url,
+            'message': data.objects[i].url,
+            'icon': './physicalweb.jpg',
+            'sound': true,
+            'wait': true,
+            'open': data.objects[i].url
+          });
+        }
+      }
+    },
+    req = http.request(options, function(res) {
+      res.setEncoding('utf-8');
+
+      var responseString = '';
+
+      res.on('data', function(data) {
+        responseString += data;
+      });
+
+      res.on('end', function() {
+        try {
+          var response = JSON.parse(responseString);
+          for (var i in response.metadata) {
+            if (response.metadata.hasOwnProperty(i)) {
+              var data = response.metadata[i];
+              console.log(chalk.underline.bgBlue(' ' + data.title + ' '));
+              console.log(chalk.gray(data.description));
+              console.log(data.displayUrl);
+              console.log();
+
+              notifier.notify({
+                'title': data.title,
+                'subtitle': data.displayUrl,
+                'message': data.description,
+                'icon': './physicalweb.jpg',
+                'contentImage': data.icon,
+                'sound': true,
+                'wait': true,
+                'open': data.url
+              });
+            }
+          }
+        } catch (e) {
+          console.log(e);
+          console.log();
+          urlOnly();
+        }
+      });
+    });
+
+  req.on('error', function(e) {
+    console.log(e);
+    console.log();
+    urlOnly();
+  });
+
+  req.write(urls);
+  req.end();
+};
