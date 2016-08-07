3cfbe3da92d5bbf1736040730f08ca0c9b672b9b
Alexis Duque <alexisd61@gmail.com>
2016-08-07 15:56:44 +0200

    Configure both Beacon V1 and V2

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -222,6 +222,7 @@ gulp.task('clean', function() {
 gulp.task('serve', ['styles'], function() {
   browserSync({
     port: 5000,
+    host: '192.168.0.11',
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
@@ -235,7 +236,7 @@ gulp.task('serve', ['styles'], function() {
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
-    // https: true,
+    https: true,
     server: {
       baseDir: ['.tmp', 'app'],
       middleware: [historyApiFallback()]
